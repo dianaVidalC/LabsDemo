3a5723b21df4a9623a46c99f8103c4925f5d0a9d
Erika Vidal <erikadianavidalcapillo@gmail.com>
2017-09-17 18:18:13 -0500

    terminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,92 @@
 $(_=> {
-    const validateForm = () => {
-        const Password = document.getElementById("input-password");
-        const confirmaPassword = document.getElementById("new-password");
-        const lostPassword = document.getElementById("lost-password");
-        const enviar = document.getElementById("enviar");
+    const validateForm = (name,email,password,newPassword,lastPassword) => {
 
-        $("#name").blur(function() {
-            if ($("#name").val() =="") {
-                $(".bad").removeClass("hide");
+        name.blur(function() {
+            if (name.val() =="") {
+                $(".name .icon-error").removeClass("hide");
+                $(".name .icon-check").addClass("hide");
+                name.addClass("error");
             } else {
-                $(".bad").addClass("hide");
-                $(".good").removeClass("hide");
+                $(".name .icon-error").addClass("hide");
+                $(".name .icon-check").removeClass("hide");
+                name.removeClass("error");
             }
         })
-        $("#input-email").blur(function() {
+        email.blur(function() {
             const patron=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
-            if ($("#input-email").val() =="") {
-                $(".bad").removeClass("hide");
+            if (email.val() =="") {
+                $(".email .icon-error").removeClass("hide");
+                $(".email .icon-check").addClass("hide");
+                email.addClass("error");
             } else {
                 if(!patron.test($("#input-email").val())) {
-                    $(".bad").removeClass("hide");
+                    $(".email .icon-error").removeClass("hide");
+                    email.addClass("error");
                 }else {
-
-                    $(".bad").addClass("hide");
-                    $(".good").removeClass("hide");
+                    $(".email .icon-error").addClass("hide");
+                    $(".email .icon-check").removeClass("hide");
+                    email.removeClass("error");
                 }
             }
         })
-        $("#input-password").blur(function() {
-            if ($("#input-password").val() =="") {
-                $(".alert").removeClass("hide");
-                $(".bordes input").addClass("color-red");
+        password.blur(function() {
+            if (password.val().length<6) {
+                $(".input-password .alert").removeClass("hide");
+                $(".input-password .icon-error").removeClass("hide");
+                $(".input-password .icon-check").addClass("hide");
+                password.addClass("error");
             } else {
-                $(".alert").addClass("hide");
-                $(".bordes input").removeClass("color-red");
+                if (password.val() === "123456" || password.val() === "abcdef") {
+                    $(".input-password .alert").addClass("hide");
+                    $(".input-password .icon-error").addClass("hide");
+                    $(".input-password .icon-check").removeClass("hide");
+                    password.removeClass("error");
+                    $(".easy-pass").removeClass("hide");
+                    $(".bar-messages").removeClass("hide");
+                }else {
+                    $(".easy-pass").addClass("hide");
+                    $(".bar-messages").addClass("hide");
+                    $(".input-password .icon-check").removeClass("hide");
+                }
+            }
+        })
+        newPassword.blur(function() {
+            if (newPassword.val()!= password.val() || newPassword.val().length===0) {
+                $(".new-password .alert").removeClass("hide");
+                $(".new-password .icon-error").removeClass("hide");
+                $(".new-password .icon-check").addClass("hide");
+                newPassword.addClass("error");
+            } else {
+                $(".new-password .alert").addClass("hide");
+                $(".new-password .icon-error").addClass("hide");
+                $(".new-password .icon-check").removeClass("hide");
+                newPassword.removeClass("error");
+            }
+        })
+        lastPassword.blur(function() {
+            if (lastPassword.val().length===0) {
+                $(".last-password .icon-error").removeClass("hide");
+                $(".last-password .icon-check").addClass("hide");
+                lastPassword.addClass("error");
+            } else {
+                $(".last-password .alert").addClass("hide");
+                $(".last-password .icon-error").addClass("hide");
+                $(".last-password .icon-check").removeClass("hide");
+                lastPassword.removeClass("error");
             }
         })
     }
-    validateForm();
+    $("#enviar").on("click",(e)=>{
+        e.preventDefault();
+        if($(".icon-error").hasClass("hide")){
+            $(".save").removeClass("hide");
+            $(".nosave").addClass("hide");
+        }
+        if($(".icon-check").hasClass("hide")){
+            $(".save").addClass("hide");
+            $(".nosave").removeClass("hide");
+        }
+    })
+    validateForm($("#name"),$("#input-email"),$("#input-password"),$("#new-password"),$("#last-password"));
 })
 
